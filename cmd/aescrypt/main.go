1990bbc0c3d318dbb8a5c73f3ee683d0beaa1c2b
Pedro F. Albanese <68971450+pedroalbanese@users.noreply.github.com>
2021-12-02 15:30:51 -0300

    Update main.go

--- cmd/aescrypt/main.go
+++ cmd/aescrypt/main.go
@@ -38,9 +38,10 @@ func main() {
 	flag.Parse()
 
 	if len(os.Args) < 2 {
-		fmt.Println("AES-GCM Encryption Tool - ALBANESE Lab (c) 2020-2021")
-		fmt.Println("Advanced Encryption Standard in Galois/Counter Mode\n")
-		fmt.Println("Usage of", os.Args[0]+":")
+		fmt.Fprintln(os.Stderr, "AES-GCM Encryption Tool - ALBANESE Lab (c) 2020-2021")
+		fmt.Fprintln(os.Stderr, "Advanced Encryption Standard in Galois/Counter Mode\n")
+		fmt.Fprintln(os.Stderr, "Usage of "+os.Args[0]+":")
+		fmt.Fprintln(os.Stderr, os.Args[0]+" [-d] [-b N] -p \"pass\" [-i N] [-s \"salt\"] -f <file.ext>")
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
